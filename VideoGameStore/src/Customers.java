ef25f5bd413d1da82d22d9b579f90929beb9a84b
t-harbin <tharbin3@students.kennesaw.edu>
2022-07-08 09:47:58 -0400

    project added

---
+++ VideoGameStore/src/Customers.java
@@ -0,0 +1,9 @@
+
+public class Customers {
+
+	public Customers() {
+		// TODO Auto-generated constructor stub
+		//Edit
+	}
+
+}
---
+++ VideoGameStore/src/ShoppingCart.java
@@ -0,0 +1,8 @@
+
+public class ShoppingCart {
+
+	public ShoppingCart() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

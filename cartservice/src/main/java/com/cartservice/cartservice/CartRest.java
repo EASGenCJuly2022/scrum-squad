7e5fbeaa16a50a1fbbab995c45a1474a06818d73
Joe Kerstetter <joseph.kerstetter@cognizant.com>
2022-08-09 10:16:38 -0400

    updated cart service
    deleted dao service and updated cartrest class

--- cartservice/src/main/java/com/cartservice/cartservice/CartRest.java
+++ cartservice/src/main/java/com/cartservice/cartservice/CartRest.java
@@ -1,25 +1,32 @@
 package com.cartservice.cartservice;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CartRest {
     @Autowired
-    private CartDao service;
+    private CartRepository service;
 
     @GetMapping("/cart")
     public List<Cart> retrieveAllCarts(){
-        return service.findAllCarts();
+        return service.findAll();
     }
 
     @PostMapping("/cart")
-    public void createCart(@RequestBody Cart cart){
+    public ResponseEntity<Object> createCart(@Validated @RequestBody Cart cart){
             Cart savedCart = service.save(cart);
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedCart.getCartId()).toUri();
+        return ResponseEntity.created(location).build();
     }
+    
 }

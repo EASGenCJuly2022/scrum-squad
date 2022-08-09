b40518595754f6a35816d633f6f64880e585ca04
brenzo44 <brenz1@lsu.edu>
2022-08-09 12:54:05 -0400

    Main working for user

--- maindriver/src/main/java/com/main/maindriver/MaindriverApplication.java
+++ maindriver/src/main/java/com/main/maindriver/MaindriverApplication.java
@@ -1,30 +1,63 @@
 package com.main.maindriver;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
-import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
 public class MaindriverApplication {
-	public static void appLoop(){
-		WebClient client = WebClient.create("http://localhost:8080");
+	public static void createPost(String f, String l, String g, String e, int p){
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/users";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("firstName", f);
+		map.put("lastName", l);
+		map.put("gender", g);
+		map.put("email", e);
+		map.put("phone", p);
+
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+		ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+		System.out.println("Created new user!");
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaindriverApplication.class, args);
+		//System.out.println(client.get().uri("/users/1").retrieve().bodyToMono(String.class).block());
+		//WebClient client = WebClient.create("http://localhost:8080");
 		Scanner scan = new Scanner(System.in);
+		Customer currentCustomer = new Customer();
 
 		while(true){
 			System.out.println("Welcome to Level Up Gaming, please enter a command: ");
-			if(scan.nextLine().equals("customer")){
-				System.out.println("Enter your customer ID to continue: ");
-				int id = Integer.parseInt(scan.nextLine());
-				String uri = String.format("/users/%d", id);
-				System.out.println(client.get().uri(uri).retrieve().bodyToMono(String.class).block());
+			String choice = scan.nextLine().toLowerCase();
+			if(choice.equals("customer")){
+				System.out.println("Enter your first and last name to continue: ");
+				String first = scan.nextLine();
+				String last = scan.nextLine();
+				String uri = String.format("http://localhost:8080/users/find/%s_%s", first, last);
+				RestTemplate restT = new RestTemplate();
+				currentCustomer = restT.getForObject(uri, Customer.class);
+				System.out.println(currentCustomer);
+				//System.out.println(client.get().uri(uri).retrieve().bodyToMono(String.class).block());
 			}
-			if(scan.nextLine().equals("create")){
+			else if(choice.equals("create")){
 				System.out.println("Creating a customer...");
 				System.out.println("Enter your first name: ");
 				String first = scan.nextLine();
@@ -36,28 +69,17 @@ public class MaindriverApplication {
 				String email = scan.nextLine();
 				System.out.println("Enter your phone: ");
 				int number = Integer.parseInt(scan.nextLine());
-				JSONObject job = new JSONObject();
-				job.put("firstName", first);
-				job.put("lastName", last);
-				job.put("gender", gender);
-				job.put("email", email);
-				job.put("number", number);
+				createPost(first, last, gender, email, number);
 				
 			}
-			else if(scan.nextLine().equals("exit")){
+			else if(choice.equals("exit")){
 				scan.close();
-				break;
+				System.exit(1);
+			}
+			else if(choice.equals("current")){
+					System.out.printf("Current customer is: %s %s, with id: %d\n", currentCustomer.getFirstName(), currentCustomer.getLastName(), currentCustomer.getId());
 			}
 		}
-	}
-	
-
-	public static void main(String[] args) {
-		SpringApplication.run(MaindriverApplication.class, args);
-		//WebClient client = WebClient.create("http://localhost:8080");
-		//System.out.println(client.get().uri("/users/1").retrieve().bodyToMono(String.class).block());
-
-		appLoop();
 	}
 
 }
